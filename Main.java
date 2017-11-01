19bef3af642a556211b1360bf4798be226ff7edb
jindavil <32707134+jindavil@users.noreply.github.com>
2017-11-01 17:50:44 +0200

    Add files via upload
    HM OOp1

---
+++ Main.java
@@ -0,0 +1,9 @@
+package shaffel1;
+public class Main {
+    public static void main(String[] args) {
+        chelovek eslinetlejki = new chelovek();
+        chelovek esliestlejak = new chelovek();
+        eslinetlejki.setLejka(false);
+        esliestlejak.setLejka(true);
+    }
+}
